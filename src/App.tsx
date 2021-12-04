1951261cd536f7cafaa9fc36ed7b1025ba5499d5
Carlos Di Matteo <carlosdima97@gmail.com>
2021-12-04 14:39:47 -0500

    initial cmc adapter implementation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { BigNumber, ethers } from 'ethers'
 import * as React from 'react'
+import { CoinMarketCapAPI } from './adapters/CoinMarketCap'
 import './App.css'
 import {conversionTable,Unit} from './constants/table'
 import {safeConvertFrom} from './utils/converter'
 export default function App() {
   const [value,setValue] = React.useState("0")
   const [currentFactor,setCurrentFactor] = React.useState({num:1,str:"1"})
+  const [price,setPrice] = React.useState<{[key:string]:number}>({})
+ 
+  React.useEffect( ()=>{
+    async function getPrice() {
+      const apiData = await  CoinMarketCapAPI.getPrices(['eth','raid','dai'])
+      console.log({apiData})
+      setPrice(apiData)
+    }
+    if(!Object.values(price).length){
+      getPrice()
+    }
+  } ,[])
   return (
     <main>
-    Convert Units (ETH)
+    Convert Units (ETH : {JSON.stringify(price)})
     <br/>
       {Object.entries(conversionTable).map(([name,factor])=>{
         const typedFactor = factor as Unit;
