60d05c58fb3e7bc35d6c123472fefba76f26389d
Carlos Di Matteo <carlosdima97@gmail.com>
2021-12-05 15:59:45 -0500

    modified content on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
-import { BigNumber, ethers } from 'ethers';
-import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './theme/GlobalStyles';
-import { conversionTable, Unit } from './constants/table';
 import { ConverterTheme } from './theme';
-import { safeConvertFrom } from './utils/converter';
 import { Header } from './containers/Header';
 import { Content } from './containers/Content';
-import { useWeb3 } from './hooks/useWeb3';
+import { ConverterContent } from './containers/Converter';
+import { PriceCardsContainer } from './containers/Prices/Prices.styles';
 export default function App() {
-	const { connect, disconnect, provider, getETHBalance, ready } = useWeb3();
-	// useEffect( ()=>{
-	//   async function getPrice() {
-	//     const apiData = await  CoinMarketCapAPI.getPrices(['eth','raid','dai'])
-	//     console.log({apiData})
-	//     setPrice(apiData)
-	//   }
-	//   if(!Object.values(price).length){
-	//     getPrice()
-	//   }
-	// } ,[])
-
-	// useEffect(() => {
-	// 	async function setEthBalance() {
-	// 		const { wei, eth } = await getETHBalance();
-	// 		setValue(wei?.toString());
-	// 		console.log('set eth', wei.toString());
-	// 	}
-	// 	if (ready) {
-	// 		setEthBalance();
-	// 	}
-	// }, [ready]);
 	return (
 		<ThemeProvider theme={ConverterTheme}>
 			<GlobalStyle />
+			<Header />
 			<Content>
-				<Header />
+				<>
+					<ConverterContent />
+					<PriceCardsContainer />
+				</>
 			</Content>
 		</ThemeProvider>
 	);
