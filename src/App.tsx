606911155f70e46db9867ff62deb49104edec2fe
Carlos Di Matteo <carlosdima97@gmail.com>
2021-12-05 17:18:40 -0500

    added web3 context provider and usage

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import { Header } from './containers/Header';
 import { Content } from './containers/Content';
 import { ConverterContent } from './containers/Converter';
 import { PriceCardsContainer } from './containers/Prices/Prices.styles';
+import { Prices } from './containers/Prices';
+import { Web3Provider } from './hooks/useWeb3';
 export default function App() {
 	return (
-		<ThemeProvider theme={ConverterTheme}>
-			<GlobalStyle />
-			<Header />
-			<Content>
-				<>
-					<ConverterContent />
-					<PriceCardsContainer />
-				</>
-			</Content>
-		</ThemeProvider>
+		<Web3Provider>
+			<ThemeProvider theme={ConverterTheme}>
+				<GlobalStyle />
+				<Header />
+				<Content>
+					<>
+						<ConverterContent />
+						<Prices />
+					</>
+				</Content>
+			</ThemeProvider>
+		</Web3Provider>
 	);
 }
